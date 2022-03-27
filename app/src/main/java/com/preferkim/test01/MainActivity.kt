5b24a711599b28767f42168b83df0c9cac7d0130
Balparang <shineecard5@gmail.com>
2022-03-27 18:19:06 +0900

    [Fix] DataBinding 에러 해결: implementation 'com.google.android.material:material:1.5.0-alpha02'로 변경 및 SDK 29로 다운그레이드

--- app/src/main/java/com/preferkim/test01/MainActivity.kt
+++ app/src/main/java/com/preferkim/test01/MainActivity.kt
@@ -2,13 +2,23 @@ package com.preferkim.test01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.preferkim.test01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.diceStartButton.setOnClickListener {
+            Toast.makeText(this, "주사위 GO!", Toast.LENGTH_LONG).show()
+        }
     }
 }
